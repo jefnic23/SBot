fd947db8808d29f4c1cdb6bb5e73fafc857b2c32
Jeff <jefnic23@gmail.com>
2022-09-02 21:33:32 -0400

    db update

--- sbot.py
+++ sbot.py
@@ -1,9 +1,21 @@
-from datetime import datetime
-from tqdm import tqdm
+import os
+import warnings
+
 import pandas as pd
 import pybaseball
-import warnings
-import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from tqdm import tqdm
+
+
+def db_engine():
+    load_dotenv()
+    USER = os.getenv('USER')
+    PSWD = os.getenv('PSWD')
+    HOST = os.getenv('HOST')
+    PORT = os.getenv('PORT')
+    NAME = os.getenv('NAME')
+    return create_engine(f'postgresql://{USER}:{PSWD}@{HOST}:{PORT}/{NAME}')
 
 
 def mlbToFg(mlbam):
@@ -19,57 +31,50 @@ def mlbToFg(mlbam):
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
 
-    while True:
-        year = input("Enter a year from 2015 to present: ")
-        try:
-            year = int(year)
-        except ValueError:
-            print('Not a valid year')
-            continue
-        if 2015 <= year <= datetime.now().year:
-            break
-        else:
-            print('Not a valid year')
-
     # initialize progress bar
-    pbar = tqdm(total=70)
+    pbar = tqdm(total=3, position=0, desc='Overall')
 
-    while True:
-        # get player id data and read into a pandas dataframe
-        url = "https://raw.githubusercontent.com/chadwickbureau/register/master/data/people.csv"
-        lookup = pd.read_csv(url, index_col="key_mlbam")
-        pbar.update(10)
+    # make directory to house leaderboards
+    if not os.path.exists('data'):
+        os.mkdir('data')
 
-        # locate savant data and read into pandas dataframe
-        dir = os.path.abspath(f'../baseball_savant/data/savant_{year}.csv')
-        sv = pd.read_csv(dir)
-        pbar.update(10)
+    # get player id data and read into a pandas dataframe
+    url            = "https://raw.githubusercontent.com/chadwickbureau/register/master/data/people.csv"
+    lookup         = pd.read_csv(url, index_col="key_mlbam")
+    lookup['name'] = lookup['name_first'] + ' ' + lookup['name_last']
+    pbar.update(1)
 
+    # select necessary columns from database
+    columns = [
+        'game_year',
+        'on_1b',
+        'on_2b',
+        'game_pk',
+        'at_bat_number',
+        'bb_type'
+    ]
+
+    # get savant data from postgres and read into pandas dataframe
+    engine = db_engine()
+    df     = pd.read_sql('baseball_savant', engine, columns=columns)
+    pbar.update(1)
+
+    with pd.ExcelWriter('data/SBot_leaderboards.xlsx') as writer:
+        for year in tqdm(range(df['game_year'].min(), df['game_year'].max()+1), position=1, desc="Leaderboards"):
         # drop all rows where there isn't a stolen base opportunity (runner on first, second base empty), then group by runner
-        sv = sv[(~sv['on_1b'].isna()) & (sv['on_2b'].isna())]
-        sv = sv.groupby(['on_1b', 'game_pk', 'at_bat_number']).size().groupby(level=0).size().reset_index().rename(columns={0: 'Opportunities'})
-        pbar.update(10)
+            sv = df[(~df['on_1b'].isna()) & (df['on_2b'].isna())]
+            sv = sv.groupby(['on_1b', 'game_pk', 'at_bat_number']).size().groupby(level=0).size().reset_index().rename(columns={0: 'Opportunities'})
 
-        # map player ids to names
-        sv['on_1b'] = sv['on_1b'].map(mlbToFg)
-        pbar.update(10)
+            # map player ids to names
+            sv['on_1b'] = sv['on_1b'].map(mlbToFg)
 
-        # get stolen base and caught stealing data from fangraphs
-        fg = pybaseball.batting_stats(year, stat_columns=['SB', 'CS'], qual=0)
-        pbar.update(10)
+            # get stolen base and caught stealing data from fangraphs
+            fg = pybaseball.batting_stats(year, stat_columns=['SB', 'CS'], qual=0)
 
-        # merge fangraphs and statcast data
-        df = pd.merge(sv, fg, left_on="on_1b", right_on="IDfg")
-        df['Attempts'] = df['SB'] + df['CS']
-        df['Success Rate'] = df['SB'] / df['Attempts']
-        df['SBot'] = df['Attempts'] / df['Opportunities']
-        pbar.update(10)
-
-        # make directory to house savant data
-        if not os.path.exists('data'):
-            os.mkdir('data')
-
-        df.set_index('Name')[['Season', 'Team', 'SB', 'CS', 'Attempts', 'Success Rate', 'Opportunities', 'SBot']].to_csv(f'data/SBot_{year}.csv')
-        pbar.update(10)
-
-        break
+            # merge fangraphs and statcast data
+            df_new = pd.merge(sv, fg, left_on = "on_1b", right_on = "IDfg")
+            df_new['Attempts']     = df_new['SB'] + df_new['CS']
+            df_new['Success Rate'] = df_new['SB'] / df_new['Attempts']
+            df_new['SBot']         = df_new['Attempts'] / df_new['Opportunities']
+            df_new.set_index('Name')[['Season', 'Team', 'SB', 'CS', 'Attempts', 'Success Rate', 'Opportunities', 'SBot']].to_excel(writer, sheet_name=str(year))
+    pbar.update(1)
